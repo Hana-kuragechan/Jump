0cde04f6649bada5ebd2dccc873b088cbc7cb5eb
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-06-13 17:05:41 +0900

    やじるしで動かすまで

--- Assets/ArrowDraw.cs
+++ Assets/ArrowDraw.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,23 +13,36 @@ public class ArrowDraw : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        arrowImage.gameObject.SetActive(false);
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            clickPosition = Input.mousePosition;
-        }
-        if (Input.GetMouseButtonUp(0))
+            // 矢印の画像を出す
+            arrowImage.gameObject.SetActive(true);
+            arrowImage.rectTransform.sizeDelta = Vector2.zero;
+            clickPosition = Input.mousePosition;    // マウスのいる座標
+            // 矢印の画像をドラッグ開始位置に移動する
+            arrowImage.rectTransform.position = clickPosition;
+        }   // ドラッグ開始を検出
+        else if (Input.GetMouseButton(0))
         {
-            Vector3 dist = clickPosition - Input.mousePosition;
-            float size = dist.magnitude;
-            float angleRad = Mathf.Atan2(dist.y, dist.x);
-            arrowImage.rectTransform.rotation = Quaternion.Euler(0, 0, angleRad * Mathf.Rad2Deg);
-            arrowImage.rectTransform.sizeDelta = new Vector2(size, size);
-            Debug.Log(dist);
+            
+            Vector3 dragVector = clickPosition - Input.mousePosition;
+            // ベクトルの長さを得る            
+            float size = dragVector.magnitude;
+            // 矢印をベクトルの方向に向ける
+            arrowImage.rectTransform.right = dragVector;
+            // ドラッグをたくさんしたら矢印を大きくする
+            arrowImage.rectTransform.sizeDelta = Vector2.one * size;
+        }   // ドラッグしている間処理する
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // 矢印の画像を消す
+            arrowImage.gameObject.SetActive(false);
         }
     }
 }
+
